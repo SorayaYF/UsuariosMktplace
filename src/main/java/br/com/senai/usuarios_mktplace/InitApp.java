8dc66edfa505f4f74669448e7364cdc1026b4884
SORAYA YASIN FARAG <soraya_farag@estudante.sesisenai.org.br>
2023-08-23 22:08:31 -0300

    Finalizada a funcionalidade de criação de novo usuário

--- src/main/java/br/com/senai/usuarios_mktplace/InitApp.java
+++ src/main/java/br/com/senai/usuarios_mktplace/InitApp.java
@@ -3,12 +3,9 @@ package br.com.senai.usuarios_mktplace;
 import br.com.senai.usuarios_mktplace.core.service.UsuarioService;
 
 public class InitApp {
-
+	
 	public static void main(String[] args) {
 		UsuarioService service = new UsuarioService();
-		System.out.println(new UsuarioService().removerAcentoDo("José da Silva"));
-		System.out.println(service.fracionar("José da Silva Alburquerque dos Anjos e Bragança"));
-		System.out.println(service.gerarLoginPor("José da Silva Alburquerque dos Anjos e Bragança"));
-		System.out.println(service.gerarHashDa("jose123456"));
+		service.criarPor("Jumenta Linda", "1212abcd");
 	}
 }
