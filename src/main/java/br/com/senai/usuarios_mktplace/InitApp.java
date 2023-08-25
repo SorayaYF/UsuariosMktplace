3ba30e605589f83bee33fd18cfe6d6987f95026f
SORAYA YASIN FARAG <soraya_farag@estudante.sesisenai.org.br>
2023-08-24 22:18:38 -0300

    Inicio do uso do lombok

--- src/main/java/br/com/senai/usuarios_mktplace/InitApp.java
+++ src/main/java/br/com/senai/usuarios_mktplace/InitApp.java
@@ -1,18 +1,11 @@
 package br.com.senai.usuarios_mktplace;
 
-import br.com.senai.usuarios_mktplace.core.dao.DaoUsuario;
-import br.com.senai.usuarios_mktplace.core.dao.FactoryDao;
 import br.com.senai.usuarios_mktplace.core.domain.Usuario;
 
 public class InitApp {
 
 	public static void main(String[] args) {
-		DaoUsuario dao = FactoryDao.getInstance().getDaoUsuario();
-		Usuario usuario = dao.buscarPor("asd");
-		if (usuario == null) {
-			System.out.println("Não existe usuário");
-		} else {
-			System.out.println(usuario.getNomeCompleto());
-		}
+		Usuario usuario = new Usuario("jose.silva", "José da Silva", "jose2023");
+		usuario.getLogin();
 	}
 }
